44fed1263b999c4b1d3da4b643f9c55712e3bb41
Simon Conrad <simondbconrad@gmail.com>
2024-06-05 12:36:10 -0400

    create BlogPostDetail view

--- backend/blog_api/urls.py
+++ backend/blog_api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ListBlogPosts
+from .views import ListBlogPosts, BlogPostDetail
 
 
 urlpatterns = [
-    path("blogposts/", ListBlogPosts.as_view(), name="blog-posts")
+    path("blogposts/", ListBlogPosts.as_view(), name="blog-posts"),
+    path("blogposts/<int:pk>/", BlogPostDetail.as_view(), name="blog-post-detail")
 ]
--- backend/blog_api/views.py
+++ backend/blog_api/views.py
@@ -7,3 +7,7 @@ from .serializers import BlogPostSerializer
 class ListBlogPosts(generics.ListAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+class BlogPostDetail(generics.RetrieveAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
